3bf87c46d68adeb4c592598c43f193eed5840e09
LPV <lpvillazon@gmail.com>
2024-03-16 18:51:29 +0000

    adjusted the termination condition of the EC code division loop. Works for small codes but not for v6

--- reed_solomon.py
+++ reed_solomon.py
@@ -195,9 +195,10 @@ def ec_codewords(message, ec_codewords: int):
     #print("Multiplied generator:", generator.alpha_form())
 
     # for as many times as the number of codewords in the message
+    #for step in range(len(message)):
     # OR
     # while the highest term of the result exceeds the highest term of the multiplied generator???
-    for step in range(len(message)):
+    while result.highest_power_of_x() >= generator.highest_power_of_x():
         #print("Step", step+1)
         # multiply the original generator in such a way as to guarantee that the lead term in the message
         # will be cancelled during the XOR step
@@ -360,6 +361,19 @@ def test():
     # input message codewords from the java app and try generating the correct codewords for them
     while True:
         message_string = input("\nEnter message codewords: ")
+        codewords_required = int(input("How many EC codewords to generate? "))
         message = [int(m) for m in message_string.split(",")]
         print("\nYour EC codewords:")
-        print(", ".join([str(cw) for cw in ec_codewords(message, 28)]))  # 28 EC codewords required
+        print(", ".join([str(cw) for cw in ec_codewords(message, codewords_required)]))  # 28 EC codewords required
+
+    # take hardcoded message codewords for "the quickest brownest fox you ever did see wa"
+    # and try to generate 28 codewords per block, as required by EC level H
+    # message_blocks = ["66, 215, 70, 134, 82, 7, 23, 86, 150, 54, 182, 87, 55, 66, 6",
+    #     "39, 38, 247, 118, 230, 87, 55, 66, 6, 102, 247, 130, 7, 150, 247",
+    #     "82, 6, 87, 102, 87, 34, 6, 70, 150, 66, 7, 54, 86, 82, 7",
+    #     "118, 16, 236, 17, 236, 17, 236, 17, 236, 17, 236, 17, 236, 17, 236"
+    #     ]
+    # print("\nYour EC codewords (len=28):")
+    # for block in message_blocks:
+    #     message = [int(m) for m in block.split(", ")]
+    #     print(",".join(["{:>4}".format(str(cw)) for cw in ec_codewords(message, 28)]))  # 28 EC codewords required
