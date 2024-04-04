ce17e1eb158fcb8fa95be2d13a2bfc31937221ba
lvillazon <lpvillazon@gmail.com>
2024-04-04 16:17:21 +0100

    minor tidying

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ from enum import Enum
 from visualiser import draw_grid
 from qr_code import generate_qr_code
 
-Module = Enum('Module', ['BLACK', 'WHITE'])
 
 # generate a known QR code to validate
 if __name__ == '__main__':
     print('Python QR Code generator')
-    # draw_grid(generate_qr_code('HELLO WORLd'))
     start = time.monotonic_ns()
-    qr_code = generate_qr_code('the quickest brownest fox yobrownest fox you ever u ever brownest fox you ever did see w')
+    qr_code = generate_qr_code('the quickest brownest fox')
     duration = (time.monotonic_ns() - start)//10000000
     print("Completed in {:d}ms".format(duration))
     draw_grid(qr_code)
